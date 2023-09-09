c2c6142604ec14e4e21572a27b3c1122161c1337
TechSulai786 <techsulai786@outlook.com>
2023-09-09 23:38:28 +0530

    updated

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
+const button = document.getElementById('myButton')
 
-
-//let name = prompt("Hi im tech sulai");
-//console.log( Number(name) + 10);
-
-let firstname = "Tech ";
-let lastname = "Sulai ";
-let fullname = "Legend";
-
-console.log(`${firstname}${lastname}${fullname}`);
-
-let array = ['a','b','c','d','e']
-console.log(array.pop());
-console.log(array.push('e'))
-console.log(array)
+button.addEventListener('click',displaystatus)
+function displaystatus()
+{
+    
+    let input = document.getElementById("input")
+    let city = input.options[input.selectedIndex].value
+    console.log(city)
+}
